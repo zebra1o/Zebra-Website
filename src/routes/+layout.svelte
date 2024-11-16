485fc9a0e04c157ba02a71b879f412d67dba5fc1
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-11-16 13:49:43 +0100

    feat: about section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,13 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Zebra Portfolio</title>
+	<meta name="description" content="Zebra Portfolio" />
+	<meta name="author" content="Zebra" />
+	<meta property="og:title" content="Zebra Portfolio" />
+	<meta property="og:description" content="Zebra Portfolio" />
+	<meta property="og:image" content="/icons/android-icon-192x192.png" />
+</svelte:head>
+
 {@render children()}
