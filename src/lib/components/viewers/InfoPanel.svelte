16cfae876de20b70ddfaa8f9be4411c615a16929
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2025-01-18 23:57:11 +0100

    feat: draggable info panel

--- src/lib/components/viewers/InfoPanel.svelte
+++ src/lib/components/viewers/InfoPanel.svelte
@@ -1,12 +1,44 @@
 <script lang="ts">
 	import { selectedWork } from '$lib/stores';
 	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
+	import { gsap, Draggable } from '$lib/gsap.config';
+
+	let draggableInstance: Draggable[] | null = $state(null);
+
+	let infoElement: HTMLDivElement | null = $state(null);
+
+	$effect.pre(() => {
+		if ($selectedWork && infoElement) {
+			draggableInstance = Draggable.create(infoElement, {
+				type: 'x,y',
+				bounds: '.overflow-container',
+				cursor: 'grab',
+				activeCursor: 'grabbing',
+				minimumMovement: 1,
+				onDragStart: function () {
+					gsap.to(infoElement, {
+						scale: 1,
+						boxShadow: '-5px 10px 35px 0px rgba(255, 255, 255, 0.30)',
+						duration: 0.2
+					});
+				},
+				onDragEnd: function () {
+					gsap.to(infoElement, {
+						scale: 1.02,
+						boxShadow: '-15px 20px 40px 5px rgba(255, 255, 255, 0.35)',
+						duration: 0.2
+					});
+				}
+			});
+		}
+	});
 </script>
 
 <!-- Info Panel -->
 {#if $selectedWork}
 	<div
 		id="info"
+		bind:this={infoElement}
 		class="fixed inset-x-0 bottom-3 left-3 z-50 max-w-xs border-collapse rounded-xl bg-black/20 backdrop-blur-sm sm:bottom-8 sm:left-8 sm:max-w-2xl"
 	>
 		<div
@@ -45,9 +77,3 @@
 		</div>
 	</div>
 {/if}
-
-<style>
-	#info {
-		box-shadow: -15px 20px 50px 5px rgba(255, 255, 255, 0.35);
-	}
-</style>
